e3fe2e3477d28136f24fcd460318b9fa4ced3dce
Janus Troelsen <janus@flipstone.com>
2022-10-29 14:49:10 -0500

    removeMore2

--- src/Data/OpenApi.hs
+++ src/Data/OpenApi.hs
@@ -1,25 +1,10 @@
 module Data.OpenApi (
-  module Data.OpenApi.Operation,
   module Data.OpenApi.Schema.Validation,
 ) where
 
-import Data.OpenApi.Operation
 import Data.OpenApi.Schema.Validation
 
-import Data.OpenApi.Internal
-
 -- $setup
--- >>> import Control.Lens
--- >>> import Data.Aeson
--- >>> import Data.Monoid
--- >>> import Data.Proxy
--- >>> import GHC.Generics
--- >>> import qualified Data.ByteString.Lazy.Char8 as BSL
--- >>> import Data.OpenApi.Internal
--- >>> import Data.OpenApi.Internal.Utils
--- >>> import Data.OpenApi.Lens
--- >>> :set -XDeriveGeneric
--- >>> :set -XOverloadedStrings
+-- >>> import Data.OpenApi.Internal.Schema.Validation
 -- >>> :set -XOverloadedLists
--- >>> :set -fno-warn-missing-methods
 
